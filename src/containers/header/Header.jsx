64444ee3aa24f17419ecd0f784cc3377371f42cf
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-10-12 21:00:06 -0400

    14th. commit: Header's minor fix

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,8 +6,8 @@ import './header.css'
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <h1 className="gradient__text">Dangelo Arrivillaga&apos;s Portfolio</h1>
+      <p>I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, do not hestiate to contact me.</p>
 
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address" />
